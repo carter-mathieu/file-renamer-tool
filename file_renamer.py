937ea506c56400e482f721d88c1c8805c4b86a85
carter-mathieu <cartermathieu96@gmail.com>
2024-07-12 12:29:49 -0400

    setup app skeleton

---
+++ file_renamer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# file_renamer.py
+
+"""This module provides the File Renamer Too entry-point script."""
+
+from file_renamer.app import main
+
+if __name__ == "__main__":
+    main()
---
+++ file_renamer/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# file_renamer/__init__.py
+
+"""This module provides the file_renamer package."""
+
+__version__ = "0.1.0"
